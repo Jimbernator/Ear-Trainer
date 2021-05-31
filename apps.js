28e0df4141551c49bc72b815ea43f5f6929b7f74
Jimbernator <33914087+Jimbernator@users.noreply.github.com>
2021-05-31 21:10:33 +0100

    More buttons

--- apps.js
+++ apps.js
@@ -1,10 +1,27 @@
 'use strict'
 
 const switcher = document.querySelector('.btn');
+const switcher2 = document.querySelector('.btn2');
+const switcher3 = document.querySelector('.btn3');
 
 switcher.addEventListener('click', toggleTheme);
+switcher2.addEventListener('click', newNote);
+switcher3.addEventListener('click', repeatNote);
 
 var context = new AudioContext()
+var note = 440;
+
+function newNote()
+{
+    note = getRandomIntInclusive(110,880);
+    playSound(note, "sine");
+    this.textContent = note.valueOf();
+}
+
+function repeatNote()
+{
+    playSound(note, "sine");
+}
 
 function playSound(frequency,type)
 {
@@ -23,8 +40,7 @@ function playSound(frequency,type)
 
 function toggleTheme()
 {   
-    var freq = getRandomFrequency(110,880);
-    playSound(freq, "sine");
+    playSound(note, "sine");
 
     document.body.classList.toggle('dark-theme')
     document.body.classList.toggle('light-theme')
@@ -42,11 +58,7 @@ function toggleTheme()
     console.log('current class name: ' + className);
 }
 
-function getRandomFrequency(min, max)
-{
-    return Math.floor( min + Math.random() * (max - min) );
-}
-
+//JAM. make this evenly distributed on the exponential.
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
