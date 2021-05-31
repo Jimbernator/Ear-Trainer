706b54484040bcace35ddd95e5ccdee6d35fd6dc
Jimbernator <33914087+Jimbernator@users.noreply.github.com>
2021-05-31 18:33:09 +0100

    working fixed sound

--- apps.js
+++ apps.js
@@ -2,15 +2,44 @@
 
 const switcher = document.querySelector('.btn');
 
-switcher.addEventListener('click', function() {
+
+
+
+
+
+switcher.addEventListener('click', toggleTheme);
+
+function playSound()
+{
+    var context = new AudioContext()
+    var osc = context.createOscillator()
+    osc.type = "sine"
+    osc.frequency.value ==220
+    osc.connect(context.destination)
+    osc.start()
+
+    setTimeout(() => 
+    {
+        osc.stop();
+    }, 1000);
+}
+
+function toggleTheme()
+{
+    playSound();
+
     document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('light-theme')
 
     var className = document.body.className;
-    if(className == "light-theme") {
+    
+    if(className == "light-theme")
+    {
         this.textContent = "Dark";
     }
-    else {
+    else
+    {
         this.textContent = "Light";
     }
     console.log('current class name: ' + className);
-});
+}
