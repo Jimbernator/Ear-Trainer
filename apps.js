0e0e47b4add37b256dece03f490c00b0aebe8f6d
Jimbernator <33914087+Jimbernator@users.noreply.github.com>
2021-05-31 19:16:18 +0100

    Frequencies working

--- apps.js
+++ apps.js
@@ -13,31 +13,13 @@ function playSound()
 {
     var context = new AudioContext()
     var osc = context.createOscillator()
-    osc.type = "sine"
-    osc.frequency.value ==220
-    osc.connect(context.destination)
-    osc.start()
-
-    setTimeout(() => 
-    {
-        osc.stop();
-    }, 1000);
-}
-function playSound2()
-{
-    var context = new AudioContext()
-    var osc = context.createOscillator()
     var g=context.createGain()
     osc.type = "sine" //works
     osc.connect(g)
-    osc.frequency.value ==2200
+    osc.frequency.value =2200
     g.connect(context.destination)
     osc.start()
     g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+1)
-    // setTimeout(() => 
-    // {
-    //     osc.stop();
-    // }, 1000);
 }
 
 function example4(frequency,type)
@@ -51,10 +33,24 @@ function example4(frequency,type)
     o.start(0)
     g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+1)
 }
+function exampleMe(frequency,type)
+{
+    var context = new AudioContext()
+    var osc = context.createOscillator()
+    var g=context.createGain()
+    osc.type = type
+    //^^works
+    osc.frequency.value = frequency;
+    osc.connect(g)
+    g.connect(context.destination)
+    osc.start()
+    g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+1)
+}
 
 function toggleTheme()
 {
-    playSound2();
+    //playSound();
+    exampleMe(220, "sine");
 
     document.body.classList.toggle('dark-theme')
     document.body.classList.toggle('light-theme')
