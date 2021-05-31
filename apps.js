0a810437ca72b17065e44d65dbaa3c7b228f4eb2
Jimbernator <33914087+Jimbernator@users.noreply.github.com>
2021-05-31 19:18:13 +0100

    Refactor

--- apps.js
+++ apps.js
@@ -2,44 +2,14 @@
 
 const switcher = document.querySelector('.btn');
 
-
-
-
-
-
 switcher.addEventListener('click', toggleTheme);
 
-function playSound()
-{
-    var context = new AudioContext()
-    var osc = context.createOscillator()
-    var g=context.createGain()
-    osc.type = "sine" //works
-    osc.connect(g)
-    osc.frequency.value =2200
-    g.connect(context.destination)
-    osc.start()
-    g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+1)
-}
-
-function example4(frequency,type)
-{
-    o=context.createOscillator()
-    g=context.createGain()
-    o.type=type
-    o.connect(g)
-    o.frequency.value=frequency
-    g.connect(context.destination)
-    o.start(0)
-    g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+1)
-}
-function exampleMe(frequency,type)
+function playSound(frequency,type)
 {
     var context = new AudioContext()
     var osc = context.createOscillator()
     var g=context.createGain()
     osc.type = type
-    //^^works
     osc.frequency.value = frequency;
     osc.connect(g)
     g.connect(context.destination)
@@ -49,8 +19,7 @@ function exampleMe(frequency,type)
 
 function toggleTheme()
 {
-    //playSound();
-    exampleMe(220, "sine");
+    playSound(220, "sine");
 
     document.body.classList.toggle('dark-theme')
     document.body.classList.toggle('light-theme')
