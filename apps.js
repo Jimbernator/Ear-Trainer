281f7f63e845c8d11042846616b33f9d164322d4
Jimbernator <33914087+Jimbernator@users.noreply.github.com>
2021-05-31 18:46:41 +0100

    nicer sound working

--- apps.js
+++ apps.js
@@ -23,10 +23,38 @@ function playSound()
         osc.stop();
     }, 1000);
 }
+function playSound2()
+{
+    var context = new AudioContext()
+    var osc = context.createOscillator()
+    var g=context.createGain()
+    osc.type = "sine" //works
+    osc.connect(g)
+    osc.frequency.value ==2200
+    g.connect(context.destination)
+    osc.start()
+    g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+1)
+    // setTimeout(() => 
+    // {
+    //     osc.stop();
+    // }, 1000);
+}
+
+function example4(frequency,type)
+{
+    o=context.createOscillator()
+    g=context.createGain()
+    o.type=type
+    o.connect(g)
+    o.frequency.value=frequency
+    g.connect(context.destination)
+    o.start(0)
+    g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime+1)
+}
 
 function toggleTheme()
 {
-    playSound();
+    playSound2();
 
     document.body.classList.toggle('dark-theme')
     document.body.classList.toggle('light-theme')
